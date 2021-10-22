09e4cb2c0d7660b2df89f23012ad009bf6803ebb
ALi <a893gh@gmail.com>
2021-10-22 15:41:40 +0330

    change site style

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,25 @@
-import '../styles/globals.css'
+import Layout from "../components/Layout";
+import "../styles/globals.css";
+import "antd/dist/antd.css";
+import NextNprogress from "nextjs-progressbar";
+import { ConfigProvider } from "antd";
+import faIR from "antd/lib/locale/fa_IR";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ConfigProvider locale={faIR} direction="rtl">
+      <NextNprogress
+        color="yellow"
+        startPosition={0.6}
+        stopDelayMs={200}
+        height={3}
+        showOnShallow={true}
+      />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ConfigProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
