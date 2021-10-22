94db51ef145744f149e83af5184f1a1e6c9cdb75
siyavash <siyavash.pou@gmail.com>
2021-10-22 12:52:18 +0330

    create helper

---
+++ src/helper/contentfull.js
@@ -0,0 +1,6 @@
+import * as SDK from 'contentful'
+const contentful = SDK.createClient({
+    space: 'lks3q6k8qln0',
+    accessToken: 'OSynRVuFAOUYbFJAHaWjQb35o9BDeKAHC-RNHgLpNOo'
+})
+export default contentful
